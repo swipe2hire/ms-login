37840d263b789cff036df983c857c20784723671
swipe2hire <dineshvanilbontha@gail.com>
2025-03-20 22:12:39 -0400

    changes for created at and updated at

--- src/db-schemas/signup-schema.js
+++ src/db-schemas/signup-schema.js
@@ -8,6 +8,8 @@ const signupSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     dob:{type:String, required:true},
+    createdAt:{ type: Date, default: Date.now},
+    updatedAt:{ type: Date, default: Date.now},
     role: {
         type: String,
         enum: ["admin", "manager", "sales"],
